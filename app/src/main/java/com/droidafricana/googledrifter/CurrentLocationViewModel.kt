f5cdd9c9a20adb5f531f2788f04ee33bfaf2b4e0
Fbada006 <ferdinandkamata@yahoo.com>
2020-08-05 16:24:40 +0300

    feature: enable location

---
+++ app/src/main/java/com/droidafricana/googledrifter/CurrentLocationViewModel.kt
@@ -0,0 +1,53 @@
+package com.droidafricana.googledrifter
+
+import android.app.Application
+import android.location.Geocoder
+import androidx.lifecycle.AndroidViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.Locale
+
+class CurrentLocationViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val context = application.applicationContext
+
+    private val _locationData = CurrentLocationLiveData(application)
+
+    val locationData: CurrentLocationLiveData
+        get() = _locationData
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            map.addMarker(
+                MarkerOptions().position(latLng)
+                    .title(getAddress(latLng.latitude, latLng.longitude))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            ).showInfoWindow()
+        }
+    }
+
+    private fun getAddress(latitude: Double, longitude: Double): String? {
+        return try {
+            val myLocation = Geocoder(context, Locale.getDefault())
+            val addressList = myLocation.getFromLocation(latitude, longitude, 1)
+            val address = addressList.getOrNull(0)
+            val addressStr = address?.getAddressLine(0)
+            addressStr
+        } catch (e: IOException) {
+            e.printStackTrace()
+            context.getString(R.string.unknown_place)
+        }
+    }
+}
---
+++ app/src/main/java/com/droidafricana/googledrifter/model/DrifterLocation.kt
@@ -0,0 +1,6 @@
+package com.droidafricana.googledrifter.model
+
+data class DrifterLocation(
+    val longitude: Double,
+    val latitude: Double
+)
